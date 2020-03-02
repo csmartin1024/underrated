a7b8f819e93ec1a8fa9de3973d2cd6fc5af46498
Cody Martin <cody.martin@dymo.tech>
2020-03-01 16:42:17 -0800

    Basic deps

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to rssseload.div
         </p>
         <a
           className="App-link"
@@ -21,6 +21,10 @@ function App() {
       </header>
     </div>
   );
+}
+
+function Test() {
+  return console.log('test');
 }
 
 export default App;
